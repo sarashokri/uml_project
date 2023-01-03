96e2b713ef57d3c1b55b896a0d7f07223e2ec9a1
Fn-fn <116028559+Fn-fn@users.noreply.github.com>
2023-01-03 20:46:51 +0300

    Add files via upload

--- Payment.cs
+++ Payment.cs
@@ -2,41 +2,31 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Online_Shopping_Project
+namespace UML_Project_online_Shopping
 {
     class Payment
     {
-		private int card_number;
-		private int pin_code;
-		private string billing_address;
+      private  int card_number;
+        private int pin_code;
+        private string billing_addres;
 
-		public Payment()
-		{
-			throw new System.NotImplementedException("Not implemented");
-		}
-		public int GetCard_number()
-		{
-			return this.card_number;
-		}
-		public void SetCard_number(ref int card_number)
-		{
-			this.card_number = card_number;
-		}
-		public int GetPin_code()
-		{
-			return this.pin_code;
-		}
-		public void SetPin_code(ref int pin_code)
-		{
-			this.pin_code = pin_code;
-		}
-		public string GetBilling_address()
-		{
-			return this.billing_address;
-		}
-		public void SetBilling_address(ref string billing_address)
-		{
-			this.billing_address = billing_address;
-		}
-	}
+        public Payment(int card_number, int pin_code, string billing_addres)
+        {
+            this.Card_number = card_number;
+            this.Pin_code = pin_code;
+            this.Billing_addres = billing_addres;
+        }
+
+        public override string ToString()
+        {
+            return "Card_number : "+ Card_number+ " pin_code : " + pin_code + " billing_addres : " + Billing_addres;
+        }
+        public bool Holder(Customer c)
+        {
+            return c.Payment_info.pin_code == this.pin_code && c.Payment_info.Billing_addres == this.Billing_addres && c.Payment_info.Card_number == this.Card_number;
+        }
+        public int Card_number { get => card_number; set => card_number = value; }
+        public int Pin_code { get => pin_code; set => pin_code = value; }
+        public string Billing_addres { get => billing_addres; set => billing_addres = value; }
+    }
 }
