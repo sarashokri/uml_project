972bc01ebddb0faedc8ba6101c09e5f2a1724396
Zainaabusheikha <120421496+Zainaabusheikha@users.noreply.github.com>
2023-01-03 20:48:28 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,11 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace Online_Shopping_Project
+namespace UML_Project_online_Shopping
 {
     class Program
     {
+       public static List<Customer> all_Customer=new List<Customer>();
+        public static List<Seller> all_Seller = new List<Seller>();
+        public static List<Listings> all_Listings = new List<Listings>();
+      
+        public static List<Listings> sold_Listings = new List<Listings>();
         static void Main(string[] args)
         {
+            
             Console.WriteLine("Hello World!");
         }
     }
--- Seller.cs
+++ Seller.cs
@@ -16,31 +16,85 @@ namespace UML_Project_online_Shopping
         private List<Listings> Listings;
         void Sign_up_for_seller_account()
         {
+            Console.WriteLine("Plz Input your data like : string name, string password, string address, string email_address, string phone_number, string store_name");
+              string input = Console.ReadLine();
+            string[] all = input.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            this.Name = all[0];
+            this.password = all[1];
+            this.Address = all[2];
+            this.Email_address = all[3];
+            this.Phone_number = all[4];
+            this.Store_name = all[5];
 
         }
 
 void Adding_new_Listings()
         {
-
+            Console.WriteLine("Input All info : ");
+            Console.WriteLine("Plz Input your data like : string name_of_the_listing, string description, int price, int number_of_items");
+            string input = Console.ReadLine();
+            string[] all = input.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            Listings a = new Listings(all[0], all[1], int.Parse(all[3]), int.Parse(all[4]));
+            a.s = this;
+            this.Listings.Add(a);
+            Program.all_Listings.Add(a);
+          
         }
 
 void Delete_existing_listing()
         {
+            Console.WriteLine("Input Listings id");
+            string input = Console.ReadLine();
+            foreach (Listings l in Listings)
+            {
+                if (l.id == int.Parse(input))
+                {
+                    Listings.Remove(l);
+                    break;
 
+                }
+            }
         }
 
 void Change_info_and_price_for_existing_listing(Listings s)
         {
+            Console.WriteLine("Input Listings id");
+            string input = Console.ReadLine();
+            foreach (Listings l in Listings)
+            {
+                if (l.id == int.Parse(input))
+                {
+                    Console.WriteLine("Input new price");
+                    Listings.Remove(l);
 
+                    Console.WriteLine("Plz Input your data like : string name_of_the_listing, string description, int price, int number_of_items");
+                    input = Console.ReadLine();
+                    string[] all = input.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                    Listings a = new Listings(all[0], all[1], int.Parse(all[3]), int.Parse(all[4]));
+                    a.s = this;
+                 
+                    this.Listings.Add(a);
+                    Program.all_Listings.Add(a);
+                   
+                    break;
+
+                }
+            }
         }
 void view_all_listings()
         {
-
+            foreach (Listings l in Program.all_Listings)
+            {
+                Console.WriteLine(l.ToString());
+            }
         }
 
 void view_sold_listings_information()
         {
-
+            foreach (Listings l in Program.sold_Listings)
+            {
+                Console.WriteLine(l.ToString());
+            }
         }
 
 void Change_account_information()
@@ -53,7 +107,7 @@ void Change_account_information()
             Listings1 = new List<Listings>();
         }
 
-        public Seller(string name, string password, string address, string email_address, string phone_number, string store_name, List<Listings> listings)
+        public Seller(string name, string password, string address, string email_address, string phone_number, string store_name)
         {
             this.Name = name;
             this.password = password;
@@ -61,7 +115,7 @@ void Change_account_information()
             this.Email_address = email_address;
             this.Phone_number = phone_number;
             this.Store_name = store_name;
-            Listings1 = listings;
+            Listings1 = new List<Listings>();
         }
 
         public string Name { get => name; set => name = value; }
